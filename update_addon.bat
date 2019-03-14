812d5d7c58f08d30e22ac4bb7e18fc15c15a2dac
Alf Müller <4lf-mueller@gmx.de>
2019-03-14 20:00:39 +0100

    fixed script loading

--- update_addon.bat
+++ update_addon.bat
@@ -1,29 +1,40 @@
 @ECHO off & setlocal
 SET path=%1
+SET mypath=%~dp0
+SET scriptPath=%mypath:~0,-1%
 
 rem check for problem
-IF [%path%] == [] ECHO "There is a problem with the path..." & PAUSE & GOTO End
+IF [%path%] == [] ECHO "There is a problem with the path..." & GOTO End
 
 rem get name
 FOR %%a IN (%path:\= %) DO SET name=%%a
 
 rem check for problem
-IF [%name%] == [] ECHO "There is a problem with the name..." & PAUSE & GOTO End
+IF [%name%] == [] ECHO "There is a problem with the name..." & GOTO End
 
 rem getting id from addon_data.cmd file
-CALL addon_data.bat
+CALL %scriptPath%\addon_data.bat
 CALL SET id=%%%name%%%
 
 rem check for problem
-IF [%id%] == [] ECHO "Please insert an ID into 'addon_data.bat' for %name% (SET %name%=...)" & PAUSE & GOTO End
+IF [%id%] == [] ECHO "Please insert an ID into 'addon_data.bat' for %name% (SET %name%=...)" & GOTO End
 
+CALL SET typeTmp=%%type_%name%%%
+
+IF [%typeTmp%] == [] (
 SET type="ServerContent"
+) else (
+SET type=%typeTmp% & ECHO "Set type to '%type%'"
+)
 
-SET typeTmp=%%type_%name%%%
-IF NOT [%typeTmp%] == [] SET type=typeTmp & ECHO "Set type to '%type%'"
+SET addonJson=%path%\addon.json
+
+IF EXIST %addonJson% DEL %addonJson%
+
+IF EXIST %addonJson% ECHO "There is a problem with the deletion of the addonJson '%addonJson%'..." & GOTO End
 
 rem create addon.json
-CALL createAddonJson.bat %path%\addon.json %name% %type%
+CALL %scriptPath%\createAddonJson.bat %addonJson% %name% %type%
 
 SET oldDir=%CD%
 
@@ -38,7 +49,7 @@ SET/P changes=Please enter the changes:
 
 IF EXIST %gma% DEL %gma%
 
-IF EXIST %gma% ECHO "There is a problem with the deletion of the gma '%gma%'..." & PAUSE & GOTO End
+IF EXIST %gma% ECHO "There is a problem with the deletion of the gma '%gma%'..." & GOTO End
 
 rem create .gma file
 gmad.exe create -folder %path% -out %gma%
